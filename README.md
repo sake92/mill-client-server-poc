9703c4c6f7831b6ced70416b48cb7c3b010b0e00
Sakib Hadziavdic <sakib@sake.ba>
2024-12-29 11:47:50 +0100

    Use a server file lock

--- README.md
+++ README.md
@@ -15,13 +15,37 @@ java -jar ./out/server/assembly.dest/out.jar
 java -jar ./out/client/jvm/assembly.dest/out.jar
 ```
 
-
 ### ScalaNative
 ```sh
 ./mill -i show client.native.nativeLink
 ./out/client/native/nativeLink.dest/out.exe
 ```
 
+---
+## POC behavior
+By default the client just prints a version (no server interaction)
+```
+java -jar ./out/client/jvm/assembly.dest/out.jar
+Mill version 0.12
+```
+
+---
+If you give it a command it will start the server *if needed* (if it can't connect).  
+This is not launching the server as a detached/daemon process yet..
+```
+java -jar ./out/client/jvm/assembly.dest/out.jar
+Could not connect to server. Starting a new one...
+GOT MSG: Working on task 'mytask' ...
+```
+
+---
+The only "real" command you can run at the moment is "SHUTDOWN", which stops the server.
+```
+java -jar ./out/client/jvm/assembly.dest/out.jar -c SHUTDOWN
+```
+
+
+----
 
 ### Measuring JVM vs ScalaNative
 
--- server/src/ServerMain.scala
+++ server/src/ServerMain.scala
@@ -3,20 +3,52 @@ package server
 import java.io.*
 import java.net.*
 import java.nio.charset.StandardCharsets
+import java.nio.channels.FileLock
+import java.nio.file.Paths
 import java.util.concurrent.locks.ReentrantLock
 import scala.util.Using
 import mainargs.{main, arg, ParserForMethods, Flag}
 
 object ServerMain {
 
-  val taskLock = new ReentrantLock()
-  @volatile var shutdownRequested = false
+  var serverLockFile = Paths.get("server.lock")
+  // var serverLock: FileLock = scala.compiletime.uninitialized
 
   var serverSocket: ServerSocket = scala.compiletime.uninitialized
+  @volatile var shutdownRequested = false
+
+  val taskLock = new ReentrantLock()
 
   @main
   def run() = {
     println(s"Starting Mill Server...")
+    // acquire the server file lock
+    // there can only be **one server running at a time**
+    def runWithServerFileLock(serverCode: => Unit): Unit =
+      Using.resource(new RandomAccessFile(serverLockFile.toFile(), "rw")) { raf =>
+        Using.resource(raf.getChannel()) { ch =>
+          var attempts = 1
+          var didRun = false
+          while attempts <= 5 do {
+            val lock = ch.tryLock()
+            if lock == null then {
+              attempts += 1
+              println("Server file lock is locked, retrying in 1s...")
+              Thread.sleep(1000)
+            } else {
+              // all good, we can proceed
+              didRun = true
+              serverCode
+            }
+          }
+          if !didRun then println("Server file lock is locked by another server instance, gave up after 5 tries")
+        }
+      }
+
+    runWithServerFileLock(runServer())
+  }
+
+  def runServer(): Unit = {
     serverSocket = new ServerSocket(9999)
     println(s"Started Mill Server")
     try {
@@ -29,8 +61,10 @@ object ServerMain {
     } catch {
       case e: SocketException =>
         // on shutdown (server socket .close()) exception is thrown
-        if e.getMessage() == "Socket closed" then println("Exiting server..")
-        else e.printStackTrace()
+        if e.getMessage() == "Socket closed" then {
+          println("Exiting server..")
+          // serverLock.release()
+        } else e.printStackTrace()
     }
   }
 
