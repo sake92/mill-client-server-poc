9155be5b1233323e75cb2d8dc5b29f728d4a0563
Sakib Hadziavdic <sakib@sake.ba>
2024-12-29 09:15:21 +0100

    Basic server to client comms

--- client/src/ClientMain.scala
+++ client/src/ClientMain.scala
@@ -1,6 +1,12 @@
 package client
 
 import mainargs.{main, arg, ParserForMethods, Flag}
+import java.net.UnixDomainSocketAddress
+import java.nio.channels.SocketChannel
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import scala.io.StdIn
+import java.nio.ByteBuffer
 
 object ClientMain {
 
@@ -11,7 +17,47 @@ object ClientMain {
   ) = {
     command match {
       case Seq("--version") => println("Mill version 0.12")
-      case _                => println("Hello!")
+      case _                => executeServerCommand()
+    }
+  }
+
+  def executeServerCommand(): Unit = {
+    val socketChannel = connectToServer("./out/serversocket")
+    var reading = true
+    while (reading) {
+      readMessageFromSocket(socketChannel) match {
+        case Some(msg) =>
+          println(s"GOT MSG: ${msg}")
+          if msg == "Done!" then reading = false
+        case None =>
+      }
+      Thread.sleep(100)
+    }
+  }
+
+  def connectToServer(socketPath: String): SocketChannel = {
+    val socketAddress = UnixDomainSocketAddress.of(socketPath)
+    try {
+      SocketChannel.open(socketAddress)
+    } catch {
+      case e: java.net.ConnectException =>
+        println("Could not connect to server. Starting a new one...")
+        val res = os.spawn(cmd = ("java", "-jar", "out/server/assembly.dest/out.jar"))
+        println(res.wrapped.info())
+        Thread.sleep(1000) // wait for server to start
+        SocketChannel.open(socketAddress)
+    }
+  }
+
+  def readMessageFromSocket(channel: SocketChannel) = {
+    val buffer = ByteBuffer.allocate(1024)
+    val bytesRead = channel.read(buffer)
+    Option.when(bytesRead > 0) {
+      val bytes = Array.ofDim[Byte](bytesRead)
+      buffer.flip();
+      buffer.get(bytes);
+      val message = new String(bytes);
+      message
     }
   }
 
--- server/src/ServerMain.scala
+++ server/src/ServerMain.scala
@@ -1,19 +1,65 @@
 package server
 
 import mainargs.{main, arg, ParserForMethods, Flag}
+import java.nio.ByteBuffer
+import java.nio.file.Files
+import java.net.UnixDomainSocketAddress
+import java.nio.channels.ServerSocketChannel
+import java.net.StandardProtocolFamily
+import java.nio.charset.StandardCharsets
+import scala.util.Using
+import java.nio.channels.SocketChannel
 
 object ServerMain {
 
   @main
-  def run(
-      @arg(short = 'c', doc = "Command to execute")
-      command: Seq[String] = Seq("--version")
-  ) = {
-    command match {
-      case Seq("--version") => println("Mill version 0.12")
-      case _                => println("Hello!")
+  def run() = {
+    log(s"Starting Mill Server...")
+    val address = UnixDomainSocketAddress.of("./out/serversocket")
+    Files.deleteIfExists(address.getPath())
+    Using.resource(ServerSocketChannel.open(StandardProtocolFamily.UNIX)) { serverSocketChannel =>
+      serverSocketChannel.bind(address)
+      while (true) {
+        // serverSocketChannel.configureBlocking(true)
+        log("Waiting for a new client...")
+        val clientSocketChannel = serverSocketChannel.accept()
+        log("A new client connected!")
+        handleNewClient(clientSocketChannel)
+      }
+    }
+    log("Exiting server..")
+    Files.deleteIfExists(address.getPath())
+  }
+
+  def handleNewClient(clientSocketChannel: SocketChannel) = {
+    var done = false
+    val task: Runnable = () => {
+      // write something to client
+      log("Writing hello..")
+      writeToChannel(clientSocketChannel, "Hello!")
+      Thread.sleep(1_000)
+      log("Writing task..")
+      writeToChannel(clientSocketChannel, "Working on a task...")
+      Thread.sleep(5_000)
+      writeToChannel(clientSocketChannel, "Done!")
+      clientSocketChannel.close()
+    }
+    new Thread(task).start()
+  }
+
+  def writeToChannel(channel: SocketChannel, msg: String) = {
+    val buffer = ByteBuffer.allocate(1024)
+    buffer.clear()
+    buffer.put(msg.getBytes())
+    buffer.flip()
+    while (buffer.hasRemaining()) {
+      channel.write(buffer)
     }
   }
 
-  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+  def log(str: String) =
+    os.write.append(os.pwd / "server.txt", s"${str}\n")
+
+  def main(args: Array[String]): Unit =
+    ParserForMethods(this).runOrExit(args)
 }
