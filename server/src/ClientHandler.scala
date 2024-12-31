7ac3c1f1e01c6c77e110deadd66716e248d7ffad
Sakib Hadziavdic <sakib@sake.ba>
2024-12-31 17:39:17 +0100

    Fix server

--- server/src/ClientHandler.scala
+++ server/src/ClientHandler.scala
@@ -11,43 +11,45 @@ class ClientHandler(socket: Socket) extends Runnable {
   override def run(): Unit = try {
     var reading = true
     val inputStream = socket.getInputStream
-    while reading && inputStream.available() > 0 do {
-      val size = inputStream.read()
-      val msgBytes = inputStream.readNBytes(size)
-      val msg = upickle.default.readBinary[ClientMessage](msgBytes)
-      msg match
-        case ClientMessage.ExecuteCommand("noop") =>
-          // this is to test how quick is the round-trip from client-server-client
-          sendMessageToClient(ServerMessage.Done())
-        case ClientMessage.ExecuteCommand("subprocess") =>
-          // non-interactive process, just run to completion
-          sendMessageToClient(ServerMessage.RunSubprocess(Seq("java", "--version")))
-          sendMessageToClient(ServerMessage.Done())
-        case ClientMessage.ExecuteCommand("interactiveSubprocess") =>
-          // interactive process, reads from user input or whatever
-          val command =
-            if Properties.isWin then Seq("powershell", "Read-Host", "\"Enter input\"")
-            else Seq("read", "-p", "\"Enter input: \"", "user_input")
-          sendMessageToClient(ServerMessage.RunSubprocess(command))
-          sendMessageToClient(ServerMessage.Done())
-        case ClientMessage.ExecuteCommand(taskName) =>
-          if taskName == "task1" || taskName == "task2" then
-            Tasks.runWithLock(taskName, sendMessageToClient) {
-              sendMessageToClient(ServerMessage.Println(s"Working on task '${taskName}' ..."))
-              Thread.sleep(5_000) // busy working on task
-              sendMessageToClient(ServerMessage.Println(s"Task '${taskName}' is done"))
-              sendMessageToClient(ServerMessage.Done())
-              reading = false
-            }
-          else {
-            sendMessageToClient(ServerMessage.Println(s"Error: unknown task '${taskName}'"))
+    while reading do {
+      if inputStream.available() > 0 then {
+        val size = inputStream.read()
+        val msgBytes = inputStream.readNBytes(size)
+        val msg = upickle.default.readBinary[ClientMessage](msgBytes)
+        msg match
+          case ClientMessage.ExecuteCommand("noop") =>
+            // this is to test how quick is the round-trip from client-server-client
             sendMessageToClient(ServerMessage.Done())
-          }
-        case ClientMessage.Shutdown() =>
-          println("Shutdown requested.")
-          sendMessageToClient(ServerMessage.Done())
-          ServerMain.shutdownRequested = true
-          ServerMain.serverSocket.close()
+          case ClientMessage.ExecuteCommand("subprocess") =>
+            // non-interactive process, just run to completion
+            sendMessageToClient(ServerMessage.RunSubprocess(Seq("java", "--version")))
+            sendMessageToClient(ServerMessage.Done())
+          case ClientMessage.ExecuteCommand("interactiveSubprocess") =>
+            // interactive process, reads from user input or whatever
+            val command =
+              if Properties.isWin then Seq("powershell", "Read-Host", "\"Enter input\"")
+              else Seq("read", "-p", "\"Enter input: \"", "user_input")
+            sendMessageToClient(ServerMessage.RunSubprocess(command))
+            sendMessageToClient(ServerMessage.Done())
+          case ClientMessage.ExecuteCommand(taskName) =>
+            if taskName == "task1" || taskName == "task2" then
+              Tasks.runWithLock(taskName, sendMessageToClient) {
+                sendMessageToClient(ServerMessage.Println(s"Working on task '${taskName}' ..."))
+                Thread.sleep(5_000) // busy working on task
+                sendMessageToClient(ServerMessage.Println(s"Task '${taskName}' is done"))
+                sendMessageToClient(ServerMessage.Done())
+                reading = false
+              }
+            else {
+              sendMessageToClient(ServerMessage.Println(s"Error: unknown task '${taskName}'"))
+              sendMessageToClient(ServerMessage.Done())
+            }
+          case ClientMessage.Shutdown() =>
+            println("Shutdown requested.")
+            sendMessageToClient(ServerMessage.Done())
+            ServerMain.shutdownRequested = true
+            ServerMain.serverSocket.close()
+      }
       Thread.sleep(100)
     }
   } catch {
@@ -56,7 +58,7 @@ class ClientHandler(socket: Socket) extends Runnable {
   } finally {
     if !socket.isClosed then socket.close()
   }
-  
+
   private def sendMessageToClient(msg: ServerMessage): Unit =
     val bytes = upickle.default.writeBinary(msg)
     socket.getOutputStream.write(bytes.length) // size acts as a delimiter too..
